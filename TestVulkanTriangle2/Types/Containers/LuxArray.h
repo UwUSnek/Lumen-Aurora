fd4543261b12addd46d91df552643e6130621161
UwU_Snek <EXPLOSION>
2020-05-28 19:29:58 +0200

    Fixed crash in LuxArray

--- TestVulkanTriangle2/Types/Containers/LuxArray.h
+++ TestVulkanTriangle2/Types/Containers/LuxArray.h
@@ -1,12 +1,8 @@
 
-//Copyright 2020 Edoardo Manente
-//Version 1.0
-
-
 
 #pragma once
-#include <vector>
 #include "Types/Integers/Integers.h"
+#include <vector>
 
 #ifndef __LUXARRAY
 #define __LUXARRAY
@@ -14,44 +10,54 @@
 
 
 
-
-//Chained Random Access array
+//Lux Engine Array
 //A 64-bit index array capable of containing billions of elements without losing performances
-//The elements are not ordered, so you have to use their ID to get them
-//If you can't use a value for invalid indices, use the 'isValid' function
+//The elements are not ordered, so each element has a unique ID. The ID is returned by add functions
+//Use the isValid() function to check if an element can be used or not
 template<class vecType>
 class LuxArray {
+private://I'm sorry for the public and privare sections randomly changing, but with a different variable order, the program crashes
+	uint64 chunksDynNum;			//Allocated number of chunks
+public:
+	//Allocated number of elements
+	//This is a private member, but it's in the public section for performance reasons. Please use size() instead
+	uint64 __dynSize;
+	//Number of free elements in the array
+	//This is a private member, but it's in the public section for performance reasons. Please use usedSize() and freeSize() instead
+	uint64 __freeNum;
 private:
-	uint64 dynSize, chunksDynNum;	//Allocated number of elements and chunks
-	uint64 head, tail;				//First and last free element
-	uint64 freeNum;					//Number of free elements in the array
+	uint64 head;					//First free element
+	uint64 tail;					//Last free element
 	uint64 chunkSize;				//Size of the chunks
 	uint64 maxSize;					//Maximum number of elements in the array
-	vecType** data_;				//Array elements
-	uint64** tracker;				//Array that stores the state of each element
-
-	#define Data(index) data_[index / chunkSize][index % chunkSize]			//Get a data    element using only one index instead of index in the chunk and chunk index
-	#define Tracker(index) tracker[index / chunkSize][index % chunkSize]	//Get a tracker element using only one index instead of index in the chunk and chunk index
-
-
-
-
 public:
+	//Array's elements
+	//This is a private member, but it's in the public section for performance reasons. Use it only if you know what you're doing
+	vecType** __data;
+	//Array that stores the state of each element
+	//This is a private member, but it's in the public section for performance reasons. Use it only if you know what you're doing
+	uint64** __tracker;
+
+	#define __Data(index) __data[index / chunkSize][index % chunkSize]			//Get a data    element using only one index instead of index in the chunk and chunk index
+	#define __Tracker(index) __tracker[index / chunkSize][index % chunkSize]	//Get a tracker element using only one index instead of index in the chunk and chunk index
+
+
+
 
 	// Constructor --------------------------------------------------------------------------------------------------------- //
 
 
-	//size		initial size of the array. Max 0x 7FFF FFFF FFFF FFFE (wtf ~ 9.2 quintillion)
-	//chunkSize 	number of elements allocated when the array grows. Default at 0x FFFF (65535)
-	//maxSize		the maximum size the array can have. Default at 0x FFFF FFFF (~ 4.3 billion)
+	//*   size: initial size of the array. Max 0x 7FFF FFFF FFFF FFFE (wtf ~ 9.2 quintillion)
+	//*   chunkSize: number of elements allocated when the array grows. Default at 0x FFFF (65535)
+	//*   maxSize: the maximum size the array can have. Default at 0x FFFF FFFF (~ 4.3 billion)
 	//The number of chunks depends on their size and the maximum size of the array (chunks = maxSize / chunkSize)
 	LuxArray(/*uint64 _size = 0,*/ uint64 _chunkSize = 0xFFFF, uint64 _maxSize = 0xFFFFffff) {
 		//resize(_size);
 		chunkSize = _chunkSize;													//Set size of the chunks
 		maxSize = _maxSize;														//Set maximum size of the array
-		data_ = (vecType**)malloc(sizeof(vecType*) * (maxSize / chunkSize));	//Allocate data array
-		tracker = (uint64**)malloc(sizeof(uint64*) * (maxSize / chunkSize));	//Allocate tracker array
-		chunksDynNum = dynSize = freeNum = 0;									//Initialize number of chunks, number of elements and number of free elements							
+		__data = (vecType**)malloc(sizeof(vecType*) * (maxSize / chunkSize));	//Allocate data array
+		__tracker = (uint64**)malloc(sizeof(uint64*) * (maxSize / chunkSize));	//Allocate tracker array
+		chunksDynNum = __dynSize = __freeNum = 0;								//Initialize number of chunks, number of elements and number of free elements							
 		head = tail = -1;														//Initialize head and tail
 	}
 
@@ -62,16 +68,16 @@ public:
 	//Adds an element at the end of the array, allocating a new chunk if needed
 	//Returns the ID of the element
 	uint64 append(vecType _data) {
-		if (dynSize + 1 > chunksDynNum * chunkSize) {						//If the chunk is full
-			data_[chunksDynNum] = (vecType*)malloc(sizeof(vecType) * chunkSize);		//Allocate a new data chunk
-			tracker[chunksDynNum] = (uint64*)malloc(sizeof(uint64) * chunkSize);		//Allocate a new tracker chunk
-			chunksDynNum++;													//Update the number of chunks
+		if (__dynSize + 1 > chunksDynNum * chunkSize) {								//If the chunk is full
+			__data[chunksDynNum] = (vecType*)malloc(sizeof(vecType) * chunkSize);		//Allocate a new data chunk
+			__tracker[chunksDynNum] = (uint64*)malloc(sizeof(uint64) * chunkSize);		//Allocate a new tracker chunk
+			chunksDynNum++;																//Update the number of chunks
 		}
-		Data(dynSize) = _data;												//Assign the data to the new element
-		Tracker(dynSize) = -1;												//Set the tracker as valid
+		__Data(__dynSize) = _data;														//Assign the data to the new element
+		__Tracker(__dynSize) = -1;														//Set the tracker as valid
 
-		dynSize++;															//Update the number of elements
-		return dynSize - 1;
+		__dynSize++;																	//Update the number of elements
+		return __dynSize - 1;															//Return the ID
 	}
 
 
@@ -84,15 +90,15 @@ public:
 		if (head == -1) return append(_data);			//If it has no free elements, append it
 		else {
 			if (head == tail) {								//If it has only one free element
-				Data(head2) = _data;							//Replace it
-				head = tail = Tracker(head2) = -1;				//And reset head and tail
+				__Data(head2) = _data;							//Replace it
+				head = tail = __Tracker(head2) = -1;				//And reset head and tail
 			}
 			else {											//If it has more than one
-				head = Tracker(head2);							//Update head
-				Tracker(head2) = -1;							//Update the status of the first
-				Data(head2) = _data;							//Replace it
+				head = __Tracker(head2);						//Update head
+				__Tracker(head2) = -1;							//Update the status of the first
+				__Data(head2) = _data;							//Replace it
 			}
-			freeNum--;										//Update the number of free elements
+			__freeNum--;										//Update the number of free elements
 			return head2;
 		}
 	}
@@ -105,10 +111,8 @@ public:
 	std::vector<uint64> add(std::vector<vecType> vec) {
 		std::vector<uint64> IDs;
 		IDs.resize(vec.size());								//Set the number of IDs
-		for (auto i : vec) {								//For every element of the std vector
-			IDs.push_back(add(i));								//Add it to the array and save its ID
-		}
-		return IDs;
+		for (auto i : vec) IDs.push_back(add(i));			//Add every element to the array and save its ID
+		return IDs;											//Return the IDs
 	}
 
 
@@ -123,26 +127,26 @@ public:
 			if (vec.isValid(i)) IDs.push_back(add(vec[i]));		//If it's valid, add it to the array and save its ID
 			else IDs.push_back(-1);								//If not, save -1 as ID
 		}
-		return IDs;
+		return IDs;											//Return the IDs
 	}
 
 
 
 
-	//Signs an index of the array as free, without deleting it
+	//Signs an index of the array as free, without deleting it. Returns a char for performance reasons
 	//*   index: the index to sign
 	//*   freeElm: whether to free the element or keep it in memory. Keeping it saves performances but increases memory usage
-	//*   Returns 0 if the operation succeed. If not, returns status(index)
-	signed short remove(uint64 index, bool freeElm = false) {
-		signed short r = status(index);
+	//*   Returns 0 if the operation succeed, status(index) if not
+	signed char remove(uint64 index, bool freeElm = false) {
+		signed char r = status(index);
 
 		if (r != 0) return r;							//If the index is not valid, return the error code
-		else {
-			Tracker(index) = (int64)-1;
-			if (freeElm) free(&Data(index));				//Free the element if necessary
-			if (head == (int64)-1) head = tail = index;		//If it has no free elements, initialize head and tail.
-			else tail = Tracker(tail) = index;				//If it has free elements, set the new tail and update the last free index
-			freeNum++;										//Update the number of free elements
+		else {											//If it is,
+			__Tracker(index) = -1;							//Set the index as free
+			if (freeElm) free(&__Data(index));				//Free the element if necessary
+			if (head == (uint64)-1) head = tail = index;	//If it has no free elements, initialize head and tail.
+			else tail = __Tracker(tail) = index;			//If it has free elements, set the new tail and update the last free index
+			__freeNum++;									//Update the number of free elements
 		}
 		return 0;
 	}
@@ -152,12 +156,12 @@ public:
 
 	//Sets the size of the array to 0, deleting all the elements and resetting it to the initial state
 	void clear() {
-		free(data_);															//Free data array
-		free(tracker);															//Free tracker array
-		data_ = (vecType**)malloc(sizeof(vecType*) * (maxSize / chunkSize));	//Allocate data array
-		tracker = (uint64**)malloc(sizeof(uint64*) * (maxSize / chunkSize));	//Allocate tracker array
-		chunksDynNum = dynSize = freeNum = 0;									//Reset number of chunk, number of elements, number of free elements
-		head = tail = -1;														//Reset head and tail
+		for (int i = 0; i < chunksDynNum; i++) free(__data[i]); free(__data);		//Free data array
+		for (int i = 0; i < chunksDynNum; i++) free(__tracker[i]); free(__tracker);	//Free tracker array
+		__data = (vecType**)malloc(sizeof(vecType*) * (maxSize / chunkSize));		//Allocate data array
+		__tracker = (uint64**)malloc(sizeof(uint64*) * (maxSize / chunkSize));		//Allocate tracker array
+		chunksDynNum = __dynSize = __freeNum = 0;									//Reset number of chunk, number of elements, number of free elements
+		head = tail = -1;															//Reset head and tail
 	}
 
 
@@ -165,30 +169,27 @@ public:
 
 
 	//Returns 0 if the index is used, 1 if the index is free, -1 if the index is invalid or there is an error, -2 if the index is out of range
-	signed short status(uint64 index) {
+	signed char status(uint64 index) {
 		if (index == (int64)-1)return -1;					//Invalid index
-		else if (index >= dynSize) return -2;				//Index out of range
-		else if (Tracker(index) == (uint64)-1) return 0;	//Ok
-		else if (Tracker(index) >= 0)return 1;				//Invalid element
+		else if (index >= __dynSize) return -2;				//Index out of range
+		else if (__Tracker(index) == (uint64)-1) return 0;	//Ok
+		else if (__Tracker(index) >= 0)return 1;			//Invalid element
 		else return -1;										//Unknown error
 	}
 
 
 	//Returns true if the index is used, 1 if it's free or invalid (use the 'status' function for more details)
-	bool isValid(uint64 index) { return (Tracker(index) == (uint64)-1); }
+	inline bool isValid(uint64 index) { return (__Tracker(index) == (uint64)-1); }
 
 
 	// Get ----------------------------------------------------------------------------------------------------------------- //
 
 
-	//Returns the element at the index "index", checking if it's valid. If not, returns the invalidIndexValue
-	//Alternatively, you can use the isValid(index) function
-	vecType secureGet(uint64 index, vecType& invalidIndexValue) { return (isValid(index)) ? Data(index) : invalidIndexValue; }
-	//Just standard insecure [] operator
-	vecType& operator [](int index) { return Data(index); }
+	//Use the isValid() function to check if the element can be used
+	inline vecType& operator [](int index) { return __Data(index); }
 
 	//Returns a pointer to the first element of a chunk. The elements are guaranteed to be in contiguous order
-	vecType* data(uint64 chunkIndex) { return &data_[chunkIndex][0]; }
+	inline vecType* data(uint64 chunkIndex) { return &__data[chunkIndex][0]; }
 
 	//Returns a pointer to a new array that contains all the elements in the chunks, without the invalid ones. This operation can be really slow, try to avoid using it
 	vecType* data() {
@@ -196,7 +197,7 @@ public:
 		int _i = 0;
 		for (int i = 0; i < usedSize(); i++) {
 			if (isValid(i)) {
-				arr[_i] = Data(i);
+				arr[_i] = __Data(i);
 				_i++;
 			}
 			else i++;
@@ -209,12 +210,12 @@ public:
 
 
 	//Returns the number of elements in the array, including the free ones
-	uint64 size() { return dynSize; }
+	inline uint64 size() { return __dynSize; }
 	//Returns the number of used elements
-	uint64 usedSize() { return dynSize - freeNum; }
+	inline uint64 usedSize() { return __dynSize - __freeNum; }
 	//Returns the number of free elements
-	uint64 freeSize() { return freeNum; }
+	inline uint64 freeSize() { return __freeNum; }
 };
 
 
-#endif //__CRAarray
+#endif // __LUXARRAY
