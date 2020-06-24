2df5d85595f852a57caea25fc7b3ae47e61111f9
UwU_Snek <EXPLOSION>
2020-06-24 19:22:31 +0200

    .

---
+++ TestVulkanTriangle2/LuxEngine/Types/Object/ObjectGeometry.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+#include "LuxEngine/Types/Containers/LuxArray.h"
+#include "LuxEngine/Types/Integers/Integers.h"
+#include "LuxEngine/Types/EngineTypes.h"
+#include <vulkan/vulkan.h>
+
+
+
+struct ObjectGeometry {
+	LuxCell vertices;
+	LuxCell triangles;
+	//TODO
+};
+
--- TestVulkanTriangle2/Main.cpp
+++ TestVulkanTriangle2/Main.cpp
@@ -2,59 +2,51 @@
 //^(?!(\s*\*))(?!(\s*\-\-\>))(?!(\s*\<\!\-\-))(?!(\s*\n))(?!(\s*\*\/))(?!(\s*\/\*))(?!(\s*\/\/\/))(?!(\s*\/\/))(?!(\s(using))).*$
 
 
-/*
-	LD 0
-		queue 0
-			command pool 0
-				command buffer 0
-					command 0
-					command 1
-					command 2
-					...
-				...
-			...
-		...
-	LD 1
-		...
-	...
-*/
-
-
-#ifndef NDEBUG
-#define LUX_DEBUG
-#endif
 
 #include "LuxEngine/LuxEngine.h"
 #include <filesystem>
 #include <iostream>
-
-void hh1(LuxArray<uint16> keySequenceCode) { printf("AAAAA"); }
-void hh2(LuxArray<uint16> keySequenceCode) { printf("BBBB"); }
-void hh3(LuxArray<uint16> keySequenceCode) { printf("CCC"); }
-void hh4(LuxArray<uint16> keySequenceCode) { printf("DD"); }
+#include <stdio.h>
 
 
+
+#if !defined _MSC_VER || !defined __cplusplus		//Use only C++ visual studio compiler
+#	error Visual studio C++ compiler required
+#endif
+
+
+#	ifdef _WIN64							//Windows
+#		include <direct.h>						//Windows directories
+#		define __lp_get_cwd _getcwd				//Windows directories
+#	elif defined __linux__					//Linux
+#		include <unistd.h>						//Linux directories
+#		define __lp_get_cwd getcwd				//Linux directories
+#	else									//Other operating systems
+#		error Unsupported operating system
+//#	elif defined unix || defined __unix || define __unix__	//Unix 
+#	endif
+
+
+//Rrturns the path to the current directory as a LuxString
+static inline LuxString luxGetCurrentWorkingDirectory() {
+	char buff[FILENAME_MAX];
+	__lp_get_cwd(buff, FILENAME_MAX);
+	return LuxString(buff);
+}
 
 
 
 
 int main() {
-	//std::string path = "./";
-	//for (const auto& entry : std::filesystem::directory_iterator(path))
-	//	std::cout << entry.path() << std::endl;
+	printf("%s", luxGetCurrentWorkingDirectory().begin());
 
-	//LuxString h = "hhh";
-	//h = "ciao";
-	//LuxString hh = { ' ', 'b','b','b','b', '\0' };
-	//h += hh + LuxString("gg") + "EXUPUROSION";
-	//printf("%s", h.begin());
-	
+
 	LuxInit(false);
 	LuxInputState inputStateTest{
-		LuxKeySequence{ {LUX_KEY_W | LUX_PRESS}, &hh1 },
-		LuxKeySequence{ {LUX_KEY_S | LUX_PRESS, LUX_KEY_S | LUX_RELEASE}, &hh2 } ,
-		LuxKeySequence{ {LUX_KEY_S | LUX_PRESS, LUX_KEY_A | LUX_PRESS, LUX_KEY_A | LUX_RELEASE}, &hh3 },
-		LuxKeySequence{ {LUX_KEY_S | LUX_PRESS, LUX_KEY_A | LUX_PRESS, LUX_KEY_D | LUX_PRESS}, &hh4 }
+		LuxKeySequence{ {LUX_KEY_W | LUX_PRESS}, [](LuxArray<uint16> keySequenceCode)->void {  printf("AAAAA"); }},
+		LuxKeySequence{ {LUX_KEY_S | LUX_PRESS, LUX_KEY_S | LUX_RELEASE}, [](LuxArray<uint16> keySequenceCode)->void {  printf("BBBB"); } } ,
+		LuxKeySequence{ {LUX_KEY_S | LUX_PRESS, LUX_KEY_A | LUX_PRESS, LUX_KEY_A | LUX_RELEASE}, [](LuxArray<uint16> keySequenceCode)->void {  printf("CCC"); } },
+		LuxKeySequence{ {LUX_KEY_S | LUX_PRESS, LUX_KEY_A | LUX_PRESS, LUX_KEY_D | LUX_PRESS}, [](LuxArray<uint16> keySequenceCode)->void {  printf("DD"); } }
 	};
 	luxInputSetInputState(&inputStateTest);
 
