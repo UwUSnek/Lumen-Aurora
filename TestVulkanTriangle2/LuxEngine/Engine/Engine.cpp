90bc995c5a751924ed3ae8ecb8cadf4e18d15d2b
UwU_Snek <EXPLOSION>
2020-07-04 19:21:34 +0200

    .

--- TestVulkanTriangle2/LuxEngine/Engine/Engine.cpp
+++ TestVulkanTriangle2/LuxEngine/Engine/Engine.cpp
@@ -75,12 +75,12 @@ void Engine::run(bool vUseVSync, float vFOV) {
 	*lineTest.col1 = vec4float32{ 0, 0.2, 1, 1 };
 
 	*lineTest.wd0 = 400;
-	*lineTest.wd1 = 400;
+	*lineTest.wd1 = 100;
 
 	*lineTest.x0 = 100;
-	*lineTest.y0 = 200;
+	*lineTest.y0 = 800;
 	*lineTest.x1 = 1200;
-	*lineTest.y1 = 800;
+	*lineTest.y1 = 200;
 
 
 	runCompute();
