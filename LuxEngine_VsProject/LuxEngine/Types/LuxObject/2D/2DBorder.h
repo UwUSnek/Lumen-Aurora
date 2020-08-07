81abd5fef38694654a65f4ceabe8636a020d84e1
UwU_Snek <xiaoju345@gmail.com>
2020-08-07 20:54:55 +0200

    Added debug object borders

--- LuxEngine_VsProject/LuxEngine/Types/LuxObject/2D/2DBorder.h
+++ LuxEngine_VsProject/LuxEngine/Types/LuxObject/2D/2DBorder.h
@@ -20,8 +20,11 @@ namespace lux::obj{
 			shaderLayout = LUX_DEF_SHADER_BORDER_2D;
 			this->allocate( );
 
-			core::c::shaders::cshaderNew({ core::g::wnd::gpuCellWindowOutput, core::g::wnd::gpuCellWindowSize, core::g::wnd::gpuCellWindowZBuffer, gpuCell }, shaderLayout, 1, 1, 1);
-			//core::c::shaders::cshaderNew({ core::g::wnd::gpuCellWindowOutput, core::g::wnd::gpuCellWindowSize, core::g::wnd::gpuCellWindowZBuffer, gpuCell }, shaderLayout, uint32(abs(fp.x - sp.x) + abs(fp.y - sp.y)), 1, 1);
+			//core::c::shaders::cshaderNew({ core::g::wnd::gpuCellWindowOutput, core::g::wnd::gpuCellWindowSize, core::g::wnd::gpuCellWindowZBuffer, gpuCell }, shaderLayout, 1, 1, 1);
+			//core::c::shaders::cshaderNew({ core::g::wnd::gpuCellWindowOutput, core::g::wnd::gpuCellWindowSize, core::g::wnd::gpuCellWindowZBuffer, gpuCell }, shaderLayout, sp.x - fp.x, sp.y - fp.y, 1);
+			//TODO just update the shaders when needed with the right workgroups count
+			//TODO remove useless out-of-range check in shaders
+			core::c::shaders::cshaderNew({ core::g::wnd::gpuCellWindowOutput, core::g::wnd::gpuCellWindowSize, core::g::wnd::gpuCellWindowZBuffer, gpuCell }, shaderLayout, 1980*2, 1080, 1);
 		}
 
 
@@ -30,8 +33,10 @@ namespace lux::obj{
 
 
 		void update( ) final override {
-			*(vec2i32*)((int8*)cellPtr + 0) = fp;			//    8    |    0  - 7     |    0 +
-			*(vec2i32*)((int8*)cellPtr + 8) = sp;			//    8    |    8  - 15    |    0
+			vec2f32 f = { 1920 * 2, 1080 };
+
+			*(vec2f32*)((int8*)cellPtr + 0) = fp * f;		//    8    |    0  - 7     |    0 +
+			*(vec2f32*)((int8*)cellPtr + 8) = sp * f;		//    8    |    8  - 15    |    0
 			*(uint32*)((int8*)cellPtr + 16) = (uint32)ID;	//    4    |    16 - 19    |    1 +
 			//12x trailing padding							//    12   |    20 - 31
 
