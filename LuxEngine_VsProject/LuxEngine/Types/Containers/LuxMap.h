68d5ab4f7e01e0383421544f8d4c0351b180a273
UwU_Snek <xiaoju345@gmail.com>
2020-09-23 12:12:12 +0200

    Fixed default lux::Map template parameters

--- LuxEngine_VsProject/LuxEngine/Types/Containers/LuxMap.h
+++ LuxEngine_VsProject/LuxEngine/Types/Containers/LuxMap.h
@@ -19,7 +19,9 @@ namespace lux {
 	//New elements are written over previously deleted elements, or concatenated if there are none
 	//Useful if you need to constantly add or remove elements
 	//Use the isValid() function to check if an element is valid or has been removed
-	template<class type, class iter = uint64, uint64 chunkSize = 50000 /*50 KB*/, uint64 elmPerChunk = chunkSize / sizeof(ram::ptr<int>)> class Map {
+	//template<class type, class iter = uint64, uint64 chunkSize = 50000 /*50 KB*/, uint64 elmPerChunk = chunkSize / sizeof(ram::ptr<int>)> class Map {
+	template<class type, class iter = uint64, uint64 elmPerChunk = max(50100 / sizeof(type), 4), uint64 chunkSize = sizeof(type)* elmPerChunk> class Map {
+	//TODO DO SOMETHING IF THE ELEMENT SIZE IS MORE THAN THE CHUNK SIZE
 	public:
 		//lux_sc_generate_debug_structure_body_func_only;
 		lux_sc_generate_debug_structure_body;
