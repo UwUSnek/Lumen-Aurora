f9a918c029bf0f03027a27208ca48ac6980431cc
UwU_Snek <xiaoju345@gmail.com>
2020-11-21 15:18:30 +0100

    Added exec function to lux HdCtArray

--- LuxEngine/Types/Containers/HdCtArray.hpp
+++ LuxEngine/Types/Containers/HdCtArray.hpp
@@ -9,17 +9,27 @@
 
 
 namespace lux{
+    struct dummy{};
+
     namespace __pvt{
         //Get_t iteration action
-        enum __action : int32{
-            CONT = -1,
+        enum __action : uint32{
+            CONT = (uint32)-1,
             DESC = 0,
             GETV = 1
         };
 
 
 
+        //TODO RECURSIVELY ADD PARAMETERS AND CALL FUNCTION AT THE LAST ONE
 
+        //TODO ci sono diversi tipi di superfici
+        //TODO questi tipi vengono calcolati automaticamente e salvati nei dati dell'oggetto
+        //TODO così in runtime non serve calcolarli. devono essere ricalcolati in background se l'oggetto viene modificato
+        //TODO ogni superficie ha dei punti caratteristici da cui dipendono le sue proprietà fisiche (non grafica)
+        //TODO i punti vengono calcolati dopo aver trovato il tipo e sono probabilment indici a dei punti normali
+        //TODO nn
+        //TODO
         //common virtual functions, Seq declaration, unspecialized get_t definition
         #define genGetVFunc inline virtual seq<index, type, types...>* getArr() = 0;
         template<uint32 index, class type, class ...types> struct seq;
@@ -53,19 +63,47 @@ namespace lux{
             type val;
             virtual uint64 getOriginalSize() = 0;
             inline virtual seq<index, type, types...>* getArr() final override { return this; }
-            inline void unwrCtor(const type& _val, const types&... vals){ val = _val; this->lux::__pvt::seq<index - 1, types...>::unwrCtor(vals...); }
-            inline void* getPtr(const uint32 _index){ return (getOriginalSize() - 1 - index == _index) ? (void*)&val : this->lux::__pvt::seq<index - 1, types...>::getPtr(_index); }
+            inline void ctGet(const type& _val, const types&... vals){ val = _val; this->lux::__pvt::seq<index - 1, types...>::ctGet(vals...); }
+            inline void* rtGet(const uint32 _index){ return (getOriginalSize() - 1 - index == _index) ? (void*)&val : this->lux::__pvt::seq<index - 1, types...>::rtGet(_index); }
+            template<class funcType, class retType, class ...packTypes> inline void exec(funcType func, retType* __ret, packTypes&... packVals){
+                this->lux::__pvt::seq<index - 1, types...>::template exec<funcType, retType, packTypes..., type>(func, __ret, packVals..., val);
+            }
+            template<class objType, class funcType, class retType, class ...packTypes> inline void exec(objType& object, funcType func, retType* __ret, packTypes&... packVals){
+                this->lux::__pvt::seq<index - 1, types...>::template exec<objType, funcType, retType, packTypes...>(object, func, __ret, packVals...);
+                // lux::__pvt::execObj_t<objType, funcType, retType, packTypes..., type>::exec(object, func, __ret, packVals..., val);
+            }
         };
 
 
+
+
+        template<class funcType, class retType, class ...packTypes> struct exec_t{
+            static void exec(funcType func, retType* __ret, packTypes&... packVals){ *__ret = func(packVals...); }
+        };
+        template<class funcType, class ...packTypes> struct exec_t<funcType, void, packTypes...> {
+            static void exec(funcType func, void* ret, packTypes&... packVals){ func(packVals...); }
+        };
+        template<class objType, class funcType, class retType, class ...packTypes> struct execObj_t{
+            static void exec(objType& object, funcType func, retType* __ret, packTypes&... packVals){ *__ret = (object.*func)(packVals...); }
+        };
+        template<class objType, class funcType, class ...packTypes> struct execObj_t<objType, funcType, void, packTypes...> {
+            static void exec(objType& object, funcType func, void* ret, packTypes&... packVals){ (object.*func)(packVals...); }
+        };
         //Stop at index 0 (seq specialization)
         template<class type> struct seq<0, type> :
         public get_t<CONT, 0, type>,
         public get_t<DESC, 0, type>,
         public get_t<GETV, 0, type>{
             type val;
-            void unwrCtor(const type& _val){ val = _val; }
-            inline void* getPtr(const uint32 _index){ return (void*)&val; }
+            inline virtual seq<0, type>* getArr() final override { return this; }
+            void ctGet(const type& _val){ val = _val; }
+            inline void* rtGet(const uint32 _index){ return (void*)&val; }
+            template<class funcType, class retType, class ...packTypes> inline void exec(funcType func, retType* __ret, packTypes&... packVals){
+                lux::__pvt::execObj_t<funcType, retType, packTypes..., type>::exec(func, __ret, packVals..., val);
+            }
+            template<class objType, class funcType, class retType, class ...packTypes> inline void exec(objType& object, funcType func, retType* __ret, packTypes&... packVals){
+                lux::__pvt::execObj_t<objType, funcType, retType, packTypes..., type>::exec(object, func, __ret, packVals..., val);
+            }
         };
     }
 
@@ -73,14 +111,14 @@ namespace lux{
 
 
     #define seqIndex (sizeof...(types) - 1)
-    //"Heterogeneous Data Compile Time Array" (lux::__pvt::seq interface)
+    //"Heterogeneous Data Compile Time Array" (lux::__pvt::seq uint32erface)
     //An array that can contain elements of different types
     //Size and types must be known at compile time
-    //e.g.  lux::HdCtArray<int32, float32, bool> arr = { 1, 5.10f, true };
+    //e.g.  lux::HdCtArray<uint32, float32, bool> arr = { 1, 5.10f, true };
     template<class ...types> struct HdCtArray : __pvt::seq<seqIndex, types...>{
         HdCtArray(){}
         HdCtArray(const types&... vals){
-            this->lux::__pvt::seq<seqIndex, types...>::unwrCtor(vals...);
+            this->lux::__pvt::seq<seqIndex, types...>::ctGet(vals...);
         }
         virtual uint64 getOriginalSize(){ return count(); }
 
@@ -94,8 +132,30 @@ namespace lux{
         }
 
         //Returns the address of an element as a void pointer
-        inline void* operator[](const uint32 index) { return this->lux::__pvt::seq<seqIndex, types...>::getPtr(index); }
+        inline void* operator[](const uint32 index) { return this->lux::__pvt::seq<seqIndex, types...>::rtGet(index); }
         inline uint32 count() const { return sizeof...(types); }
+
+        //Calls a function using the array elements as arguments
+        //pFunc --| The function to call
+        //pReturn | The variable where to store the return value
+        //pReturn can be omitted to ignore the return value or call void functions
+        template<class funcType, class retType> inline void exec(funcType pFunc, retType& pReturn){
+            this->lux::__pvt::seq<seqIndex, types...>::template exec<funcType, retType>(pFunc, &pReturn);
+        }
+        template<class funcType>                inline void exec(funcType pFunc                  ){
+            this->lux::__pvt::seq<seqIndex, types...>::template exec<funcType, void   >(pFunc, nullptr );
+        }
+        //Calls a function of a class instance using the array elements as arguments
+        //pObject | The class instance containing the function
+        //pFunc --| The function to call
+        //pReturn | The variable where to store the return value
+        //pReturn can be omitted to ignore the return value or call void functions
+        template<class objType, class funcType, class retType> inline void exec(objType& pObject, funcType pFunc, retType& pReturn){
+            this->lux::__pvt::seq<seqIndex, types...>::template exec<objType, funcType, retType>(pObject, pFunc, &pReturn);
+        }
+        template<class objType, class funcType               > inline void exec(objType& pObject, funcType pFunc                  ){
+            this->lux::__pvt::seq<seqIndex, types...>::template exec<objType, funcType, void   >(pObject, pFunc, nullptr );
+        }
     };
     #undef seqIndex
 }
