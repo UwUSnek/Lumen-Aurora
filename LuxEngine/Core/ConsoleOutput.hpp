621aa643b331b6fed57e08274ac6a9170b880d86
UwU_Snek <xiaoju345@gmail.com>
2021-01-12 13:21:08 +0100

    Update call backtrace output

--- LuxEngine/Core/ConsoleOutput.hpp
+++ LuxEngine/Core/ConsoleOutput.hpp
@@ -1,10 +1,10 @@
 #pragma once
 #define LUX_H_CONSOLE_OUTPUT
-#include <string>
+#include <cstring>
 #include <cstdio>
 #include "LuxEngine/macros.hpp"
 #include <exception>
-
+#include <experimental/source_location>
 
 
 
@@ -22,10 +22,46 @@ namespace lux::out{
 
 
 
+	#define callerInfo const std::experimental::source_location& caller = std::experimental::source_location::current()
+
+	template<class... types> static void printError(char* message = "UwU", types... params, callerInfo){
+		//Create output string
+		char* out = "%s\n%s\n\n  %s\"%s\"\n%s\"%s\"\n%s\"%s\"\n%s%d\n\n";
+		const char* end = "\n\n%s";
+		out = (char*)realloc(out, strlen(out) + strlen(message) + strlen(end) + 1);
+		strcat(out, message);
+		strcat(out, end);
+
+		//Output
+		char __thrName__[16]; pthread_getname_np(pthread_self(), __thrName__, 16);
+		printf(out,
+			"######################################",
+			"Lux runtime error:",
+
+			"Thread   ", __thrName__,
+			"File     ", caller.file_name(),
+			"Function ", caller.function_name(),
+			"Line     ", caller.line(),
+
+			message,
+			params...,
+
+			"######################################"
+		);
+	}
+
+
+
+
+
+
+
+
 
 	//Prints an error, specifying the line, function, file and thread where the error occurred
 	//Debug mode only. #define LUX_PAUSE_ON_ERROR to make the program stop when an error occurs
-	#define luxPrintError(...)		luxCheckCond(true, __VA_ARGS__)
+	// #define luxPrintError(...)		luxCheckCond(true, __VA_ARGS__)
+	#define luxPrintError(...)		lux::out::printError();/*BUG FIXME*/
 
 
 	//Prints a warning, specifying the line, function, file and thread
@@ -48,9 +84,10 @@ namespace lux::out{
 	//Debug mode only. #define LUX_PAUSE_ON_ERROR to make the program stop when an error occurs
 	#define luxCheckCond(condition, ...) luxRelease(;) luxDebug({																	\
 		if(condition) {																												\
-			char __thrName__[16]; pthread_getname_np(pthread_self(), __thrName__, 16);												\
-			Failure printf("Error in thread %s, file %s\nfunction %s, line %d:", __thrName__, __FILE__, __FUNCTION__, __LINE__);	\
-			Failure printf(__VA_ARGS__); fflush(stdout); NormalNoNl __lux_out_stop__												\
+			/*char __thrName__[16]; pthread_getname_np(pthread_self(), __thrName__, 16);												*/\
+			/*Failure printf("Error in thread %s, file %s\nfunction %s, line %d:", __thrName__, __FILE__, __FUNCTION__, __LINE__);	*/\
+			/*Failure printf(__VA_ARGS__); fflush(stdout); NormalNoNl __lux_out_stop__												*/\
+			lux::out::printError();/*BUG FIXME*/\
 		}																															\
 	})
 	//Prints an error if the condition is satisfied, specifying the line, function, file and thread where the error occurred
@@ -60,10 +97,11 @@ namespace lux::out{
 	//Debug mode only. #define LUX_PAUSE_ON_ERROR to make the program stop when an error occurs
 	#define luxCheckParam(condition, param, ...) luxRelease(;) luxDebug({															\
 		if(condition) {																												\
-			char __thrName__[16]; pthread_getname_np(pthread_self(), __thrName__, 16);												\
-			Failure printf("Error in thread %s, file %s\nfunction %s, line %d:", __thrName__, __FILE__, __FUNCTION__, __LINE__);	\
-			Failure printf("Invalid value passed to \"%s\" parameter of function \"%s\": %s\n", #param, __FUNCTION__, __VA_ARGS__);	\
-			fflush(stdout); NormalNoNl __lux_out_stop__																				\
+			/*char __thrName__[16]; pthread_getname_np(pthread_self(), __thrName__, 16);												*/\
+			/*Failure printf("Error in thread %s, file %s\nfunction %s, line %d:", __thrName__, __FILE__, __FUNCTION__, __LINE__);	*/\
+			/*Failure printf("Invalid value passed to \"%s\" parameter of function \"%s\": %s\n", #param, __FUNCTION__, __VA_ARGS__);	*/\
+			/*fflush(stdout); NormalNoNl __lux_out_stop__																				*/\
+			lux::out::printError();/*BUG FIXME*/\
 		}																															\
 	})
 	//Prints an error if the vulkan function does not return VK_SUCCESS, specifying the line, function, file and thread where the error occurred
