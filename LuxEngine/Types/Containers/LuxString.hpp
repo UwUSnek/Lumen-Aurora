691d5d1357c842c4b4b1ad1eb24004e4a9ba0e44
UwU_Snek <xiaoju345@gmail.com>
2021-01-24 22:40:22 +0100

    Fix String operator=

--- LuxEngine/Types/Containers/LuxString.hpp
+++ LuxEngine/Types/Containers/LuxString.hpp
@@ -24,14 +24,10 @@ namespace lux {
 		genInitCheck;
 	private:
 		luxDebug(const char* viewer;)
-		// ram::Alloc<char8> str;
 
 
 		//FIXME FIX
 		inline void cat(const char8* vString) {
-			// uint64 oldSize = Super::data.size();
-			// Super::data.realloc(Super::data.size() + size - 1);
-			// ram::cpy(vString, Super::data + oldSize - 1, size);
 			Super::resize(Super::count() - 1); //Remove \0
 			Super::cat(String(vString));
 			updateView();
@@ -47,19 +43,14 @@ namespace lux {
 
 
 
-		//String count cannot be 0. the '\0' is always present and occupies one byte
-		//TODO the elements are probably uninitialized
-		inline String(                                  ) : Super(1) { Super::data[0] = '\0'; updateView(); }
-		inline String(const char8* vString              ) : Super(strlenl(vString) + 1) { ram::cpy(vString, Super::data, Super::data.size( )); updateView(); }
-		inline String(const char8* vString, uint64 vSize) : Super(vSize)	            { ram::cpy(vString, Super::data, Super::data.size( )); updateView(); }
+		inline String(                                 ) : Super(1) { Super::data[0] = '\0'; updateView(); }
+		inline String(const char8* vString             ) : Super(strlenl(vString) + 1) { ram::cpy(vString, Super::data, Super::data.size( )); updateView(); }
+		inline String(const char8* vString, uint64 vLen) : Super(vLen)	            { ram::cpy(vString, Super::data, Super::data.size( )); updateView(); }
 
-		//Move and copy constructors
+		//Move constructor
 		inline String(String&& pString) { Super::move(pString); updateView(); }
-		// inline String(const String& pString) : Super(pString.count( ))	    { ram::cpy(pString.data, Super::data, pString.count( )); }
-		inline String(const String& pString) : Super(pString/*, false*/, {}) { updateView(); }
-
-		//TODO remove
-		// inline String(const wchar8* vString) : Super(strlenl(vString) + 1)	{ ram::cpy(vString, Super::data, Super::data.size( )); }
+		//Copy constructor
+		inline String(const String& pString) : Super(pString, {}) { updateView(); }
 
 
 
@@ -69,12 +60,8 @@ namespace lux {
 
 
 
-		// inline uint32 count( ) const override { checkInit(); return (uint32)str.count( ); }
-		//TODO move size to specific type
 		inline uint64 size(  ) const { checkInit(); return Super::data.size( ); }
-		// inline bool	  empty( ) const override { checkInit(); return !str.count( );        }
-		// inline char8* begin( ) const override { checkInit(); return str.begin( );	        }
-		// inline char8* end(   ) const override { checkInit(); return str.end( );           }
+
 
 		inline char8& operator[](const uint32 vIndex) const {
 			checkInit();
@@ -85,21 +72,13 @@ namespace lux {
 
 
 
-		// String concatenation ------------------------------------------------------------------------------------------------ //
+		// String concatenation - nya ------------------------------------------------------------------------------------------ //
 
 
 
 
 		inline void operator += (const String& pString) { checkInit(); cat(pString.Super::data); }
 		inline void operator += (const char8* vString ) { checkInit(); cat(vString); }
-
-		//TODO write itoa functions
-		// inline void operator += (const uint64 vValue)		{ checkInit(); char b[20 + 1]; _ui64toa(vValue, b, 10);	operator += (b);	}
-		// inline void operator += (const int64 vValue)		{ checkInit(); char b[20 + 1]; _i64toa(vValue, b, 10);	operator += (b);	}
-		// inline void operator += (const uint32 vValue)		{ checkInit(); char b[10 + 1]; ultoa(vValue, b, 10);		operator += (b);	}
-		// inline void operator += (const int32 vValue)		{ checkInit(); char b[10 + 1]; ltoa(vValue, b, 10);		operator += (b);	}
-		//inline void operator += (const char8 vChar)		{ checkInit(); ram::reallocBck(str, str.size( ) + 1); *str.end( ) = vChar;		}
-		// inline void operator += (const char8 vChar)		{ checkInit(); Super::data.realloc(Super::data.count( ) + 1); *Super::data.end( ) = vChar;		}
 
 
 		//TODO use sum chain struct instead of copying the string data
@@ -109,18 +88,13 @@ namespace lux {
 			vLuxString += pString;
 			return vLuxString;
 		}
-		//TODO use sum chain struct instead of copying the string data
+
 		inline String operator + (const char8* vString ) const {
 			checkInit();
 			String vLuxString(Super::data);
 			vLuxString += vString;
 			return vLuxString;
 		}
-		// inline String operator + (const uint64 vValue  ) const { checkInit(); __lp_strcat_body(vValue ); }
-		// inline String operator + (const int64 vValue   ) const { checkInit(); __lp_strcat_body(vValue ); }
-		// inline String operator + (const uint32 vValue  ) const { checkInit(); __lp_strcat_body(vValue ); }
-		// inline String operator + (const int32 vValue   ) const { checkInit(); __lp_strcat_body(vValue ); }
-		// inline String operator + (const char8 vChar    ) const { checkInit(); __lp_strcat_body(vChar  ); }
 
 
 
@@ -129,9 +103,10 @@ namespace lux {
 
 
 
+
 		//move assignment
 		inline auto& operator= (String&& pString) {
-			Super::move(pString); //BUG THIS
+			Super::move(pString);
 			updateView();
 			return *this;
 		}
@@ -140,27 +115,41 @@ namespace lux {
 		inline auto& operator= (const String& pString) {
 			checkInit(); isInit(pString);
 			Super::copy(pString);
-			// Super::data.realloc(pString.count( ));
-			// str.address = (char8*)str.cell->address;
-			// ram::cpy(pString.data, Super::data, Super::data.count( ));
 			updateView();
 			return *this;
 		}
+
 		//Copy from C-style string
 		inline auto& operator= (const char8* vString) { return operator=(String(vString)); }
 
 
 
 
+
+		//The cooler strcmp
 		inline bool operator== (const String& pString) const {
 			checkInit(); isInit(pString);
-			return ((Super::data.count( ) == pString.count( )) && (memcmp(pString.data, Super::data, Super::data.count( )) == 0));
+			return (Super::count( ) == pString.count( )) && 0 == memcmp(pString.data, Super::data, Super::count( ));
 		}
 
-
+		/**
+		 * @brief Compares the string to a C string.
+		 *		If the string lenght is known, the cmp() function can be used to avoid unnecessary strlen calls
+		 * @param vString The string to compare
+		 */
 		inline bool operator== (const char* vString) const {
 			checkInit();
-			return ((Super::data.count( ) == strlenl(vString) + 1) && (memcmp(vString, Super::data, Super::data.count( )) == 0));
+			return (Super::count( ) == strlenl(vString) + 1) && 0 == memcmp(vString, Super::data, Super::count( ));
+		}
+
+		/**
+		 * @brief operator==, but the string length can be specified
+		 * @param vString The tring to compare
+		 * @param vStrLen The length of the string, including the null terminator '\\0'
+		 */
+		inline bool cmp (const char* vString, const uint32 vStrLen) const {
+			checkInit();
+			return (Super::count( ) == vStrLen) && 0 == memcmp(vString, Super::data, Super::count( ));
 		}
 	};
 }
