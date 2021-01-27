a9165a29e69055c793730dc79bc36cab440b5ac7
UwU_Snek <xiaoju345@gmail.com>
2021-01-27 12:26:01 +0100

    Add user build options to generated builds

--- GenVSCodeBuilds.py
+++ GenVSCodeBuilds.py
@@ -2,24 +2,41 @@ import sys
 import os.path
 thisdir = os.path.dirname(os.path.abspath(__file__))
 
+
+
+
 if len(sys.argv) != 2:
     exit(-1)
 
 pdir = sys.argv[1]
 if not os.path.exists(pdir):
-    print("Unknown path")
+    print("The specified path does not exist")
     exit(-2)
-if not os.path.exists(pdir + "/.vscode"):
-    print("Unable to find the .vscode folder in the provided path")
-    exit(-3)
 
 
 
 
-pf = open(thisdir + "/Build/ProjectPath.py", "w")
-pf.write("ProjectPath = \"" + os.path.relpath(pdir, thisdir) + "\"\n")
-pf.write( "EnginePath = \"" + os.path.relpath(thisdir, pdir) + "\"")
-pf.close()
+print('The files "tasks.json" and "c_cpp_properties.json"\nin "' + os.path.abspath(pdir) + '/.vscode/" will be overwritten\nContinue?')
+if sys.stdin.read(1) == 'y':
+    if not os.path.exists(pdir + "/.vscode"):
+        os.mkdir(pdir + "/.vscode")
 
-os.system("python3 " + thisdir + "/Build/SetPlatform.py l")
-os.system("python3 " + thisdir + "/Build/SetType.py d")
+    pf = open(thisdir + "/Build/ProjectPath.py", "w")
+    pf.write("ProjectPath = \"" + os.path.relpath(pdir, thisdir) + "\"\n")
+    pf.write( "EnginePath = \"" + os.path.relpath(thisdir, pdir) + "\"")
+    pf.close()
+
+    uf = open(pdir + "/.vscode/tasks.json", "w")
+    uf.write(\
+            """[[
+                "LuxEngine/Build/lux_g++.py",
+                //Your build
+                //-l(<option>), -w(<option>), -d(<option>) and -r(<option>) can be used to specify different g++ options based on the configuration
+                //e.g. -d(-g3) -r(-g0)      //use g3 in debug mode and g0 in release
+                //e.g. -wr(-g0)             //use g0 only when building in release mode for windows
+                //The engine library is automatically linked
+            ]]""")
+    uf.close()
+
+    os.system("python3 " + thisdir + "/Build/SetPlatform.py l")
+    os.system("python3 " + thisdir + "/Build/SetType.py d")
