b070ee50d3decd7adfed40859b924c5508da1f7d
UwU_Snek <xiaoju345@gmail.com>
2021-02-01 10:49:35 +0100

    Fix auto init names

--- LuxEngine/Core/LuxAutoInit.hpp
+++ LuxEngine/Core/LuxAutoInit.hpp
@@ -1,12 +1,12 @@
 
 //Creates a function that will be called during the global variable initialization
 //This macro can only be used in the global scope of .cpp files
-#define luxAutoInit(tu)                            		\
-	namespace __pvt{									\
-		void __lainit##tu##_f();						\
-		struct __lainit##tu##_t{                   		\
-			__lainit##tu##_t() { __lainit##tu##_f(); }	\
-		};												\
-		__lainit##tu##_t __lainit##tu##_v;				\
-	}													\
-	void __pvt::__lainit##tu##_f() //{ ... } 			//Implementation
+#define luxAutoInit(tu)                            			\
+	namespace __pvt{										\
+		void __lainit_##tu##_f();							\
+		struct __lainit_##tu##_t{                   		\
+			__lainit_##tu##_t() { __lainit_##tu##_f(); }	\
+		};													\
+		__lainit_##tu##_t __lainit_##tu##_v;				\
+	}														\
+	void __pvt::__lainit_##tu##_f() //{ ... } 				//Implementation
