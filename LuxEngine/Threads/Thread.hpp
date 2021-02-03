ea22d00e381813ed9cdd80746c3ea68d235c61c1
UwU_Snek <xiaoju345@gmail.com>
2021-02-03 10:58:26 +0100

    Add member function constructor to Thread

--- LuxEngine/Threads/Thread.hpp
+++ LuxEngine/Threads/Thread.hpp
@@ -16,15 +16,20 @@
 
 namespace lux{
 	namespace __pvt{
-		template<class func_t, class ...args_ts> struct exec_thr{				//Structure containing the function call informations
+		template<class func_t, class ...args_ts> struct exec_void{				//Structure containing the function call informations
 			func_t _func;															//function pointer
 			lux::HcArray<args_ts...> _args;											//function arguments
 		};
 
-		template<class obj_t, class func_t, class ...args_ts> struct exec_obj{	//Structure containing the function call informations for member functions
+		// template<class obj_t, class func_t, class ...args_ts> struct exec_obj{	//Structure containing the function call informations for member functions
+		// 	obj_t& _obj;															//function pointer
+		// 	func_t _func;															//function pointer
+		// 	lux::HcArray<args_ts...> _args;											//function arguments
+		// };
+
+		template<class obj_t, class func_t> struct exec_void_obj{	//Structure containing the function call informations for member functions
 			obj_t& _obj;															//function pointer
 			func_t _func;															//function pointer
-			lux::HcArray<args_ts...> _args;											//function arguments
 		};
 
 
@@ -32,16 +37,34 @@ namespace lux{
 
 		//Executes a non member void function
 		template<class funcType, class ...argsTypes> static void* threadRunVoid(void* _args) {
-			using funcp = exec_thr<funcType, argsTypes...>*;
+			using funcp = exec_void<funcType, argsTypes...>*;
 			((funcp)_args)->_args.template exec<funcType>(((funcp)_args)->_func);
-			free((funcp)_args);		//Free the function data
+			delete((funcp)_args);		//Free the function data
 			return nullptr;			//Return nothing    //^ Cast to thr struct and call the function with its parameters
 		}
-
 
 		//Executes a non member void function that takes no arguments
 		template<class funcType> alwaysInline static void* threadRunVoidNoParams(void* _args) {
 			((funcType)_args)();	//Cast the function to the right type and call it
+			return nullptr;			//Return nothing
+		}
+
+
+
+
+		// //Executes a member void function
+		// template<class objType, class funcType, class ...argsTypes> static void* threadRunObjVoid(void* _args) {
+		// 	using funcp = exec_obj<objType, funcType, argsTypes...>*;
+		// 	((funcp)_args)->_args.template exec<funcType>(((funcp)_args)->obj, ((funcp)_args)->_func);
+		// 	delete((funcp)_args);		//Free the function data
+		// 	return nullptr;			//Return nothing    //^ Cast to thr struct and call the function with its parameters
+		// }
+
+		//Executes a member void function that takes no arguments
+		template<class objType, class funcType> alwaysInline static void* threadRunObjVoidNoParams(void* _args) {
+			using funcp = exec_void_obj<objType, funcType>*;
+			((((funcp)_args)->_obj).*(((funcp)_args)->_func))();	//Cast the function to the right type and call it
+			delete((funcp)_args);	//Free the function data
 			return nullptr;			//Return nothing
 		}
 	}
@@ -91,7 +114,13 @@ namespace lux{
 		 */
 		template<class funcType> alwaysInline Thread(const funcType pFunc) { operator()(pFunc); }
 
+		template<class objType, class funcType, class argType, class ...argsTypes> alwaysInline Thread(objType& obj, const funcType pFunc, const lux::HcArray<argType, argsTypes...>& pArgs) {
+			operator()(obj, pFunc, pArgs);
+		}
 
+		template<class objType, class funcType> alwaysInline Thread(objType& obj, const funcType pFunc) {
+			operator()(obj, pFunc);
+		}
 
 
 		// Operator() ---------------------------------------------------------------------------------------------------------------------------------//
@@ -105,7 +134,7 @@ namespace lux{
 		 * @param pArgs The function arguments
 		 */
 		template<class funcType, class argType, class ...argsTypes> void operator()(const funcType pFunc, const lux::HcArray<argType, argsTypes...>& pArgs) {
-			using funct = lux::__pvt::exec_thr<funcType, argType, argsTypes...>;
+			using funct = lux::__pvt::exec_void<funcType, argType, argsTypes...>;
 			auto funcd = (funct*)malloc(sizeof(funct));		//Allocate function data in the heap so that it doesnt get destroyed when the parent returns
 			funcd->_func = pFunc;							//Copy function address
 			funcd->_args = pArgs;							//Copy (((parameters references) array) by value)
@@ -122,26 +151,31 @@ namespace lux{
 
 
 		// /**
-		//  * @brief Initializes a thread with a non member void function
+		//  * @brief Initializes a thread with a member void function
 		//  * @param pFunc The function to execute
 		//  * @param pArgs The function arguments
 		//  */
 		// template<class objType, class funcType, class argType, class ...argsTypes> void operator()(objType& obj, const funcType pFunc, const lux::HcArray<argType, argsTypes...>& pArgs) {
-		// 	//FIXME
-		// 	using funct = lux::__pvt::exec_thr<funcType, argType, argsTypes...>;
-		// 	auto funcd = (funct*)malloc(sizeof(funct));
-		// 	funcd->_func = pFunc;
-		// 	funcd->_args = pArgs;
-		// 	pthread_create(&thr, nullptr, lux::__pvt::threadRunVoid<funcType, argType, argsTypes...>, funcd);
+		// 	using funct = lux::__pvt::exec_obj<objType, funcType, argType, argsTypes...>;
+		// 	auto funcd = new funct{
+		// 		._obj = obj,
+		// 		._func = pFunc,
+		// 		._args = pArgs
+		// 	};
+		// 	pthread_create(&thr, nullptr, lux::__pvt::threadRunObjVoid<objType, funcType, argType, argsTypes...>, funcd);
 		// }
-		// /**
-		//  * @brief Initializes a thread with a non member void function that takes no arguments
-		//  * @param pFunc The function to execute
-		//  */
-		// template<class objType, class funcType> void operator()(objType& obj, const funcType pFunc) {
-		// 	//FIXME
-		// 	pthread_create(&thr, nullptr, lux::__pvt::threadRunVoidNoParams<funcType>, (void*)pFunc);
-		// }
+		/**
+		 * @brief Initializes a thread with a member void function that takes no arguments
+		 * @param pFunc The function to execute
+		 */
+		template<class objType, class funcType> void operator()(objType& obj, const funcType pFunc) {
+			using funct = lux::__pvt::exec_void_obj<objType, funcType>;
+			auto funcd = new funct{
+				._obj = obj,
+				._func = pFunc
+			};
+			pthread_create(&thr, nullptr, lux::__pvt::threadRunObjVoidNoParams<objType, funcType>, funcd);
+		}
 
 
 
