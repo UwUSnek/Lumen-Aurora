547bdbbbc5502df31656b887a2f1d84cdc3c3386
UwU_Snek <xiaoju345@gmail.com>
2021-02-18 11:27:00 +0100

    test

--- LuxEngine/Types/Containers/RaArray.hpp
+++ LuxEngine/Types/Containers/RaArray.hpp
@@ -823,7 +823,7 @@ namespace lux {
 
 		/**
 		 * @brief Initializes the array by copy constructing each element from a lux::ContainerBase subclass
-		 * @param pCont The container object to copy elements from.
+		 * @param pCont The container object to copy elements from._
 		 *		It must have a compatible type and less elements than the maximum number of elements of the array you are initializing
 		 */
 		template<class eType, class iType> inline auto& operator=(const ContainerBase<eType, iType>& pCont) {
