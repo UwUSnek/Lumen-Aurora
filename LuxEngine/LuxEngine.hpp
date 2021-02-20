09d7193ff60ab3d532e7ce56309b5f610c92fa84
UwU_Snek <xiaoju345@gmail.com>
2021-02-20 21:51:09 +0100

    Remove StartLuxEngine.hpp Move start() function to LuxEngine.cpp

--- LuxEngine/LuxEngine.hpp
+++ LuxEngine/LuxEngine.hpp
@@ -8,28 +8,16 @@
 
 
 //UwU
-	//TODO
-	//The engine is initialized before anything else
-	//The "LuxEngine_config.hpp" file contains configuration macros to change the program behaviour
 
 
+#include "LuxEngine/Core/Core.hpp"
+#include "LuxEngine/Threads/Thread.hpp"
 
 
-
-
-//Names
-	//Names of function parameters passed by value start with a lowercase 'v'
-	//Names of function parameters passed by reference or pointers start with a lowercase 'p'
-
-
-
-//System
-	//Supported operating systems:
-		//Windows 10
-		//Linux
-	//Supported compilers:
-		//gcc
-	//Max (GPU + shared) memory:
-		//51200MB
-		//This value can be increased by using larger GPU buffers (LUX_CNF_GPU_STATIC_BUFFER_SIZE)
-		//Maximum memory will = (LUX_CNF_GPU_STATIC_BUFFER_SIZE * 1024)
+namespace lux{
+    void start() {
+        Thread coreThr(lux::core::run, L{ false });
+        coreThr.detach();
+        while(!core::initialized) lux::thr::self::yield();
+    }
+}
