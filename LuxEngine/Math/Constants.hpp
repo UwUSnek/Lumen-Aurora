5b908f3a6f64b9acdc668ec5e30f274c7cc8d433
Vincenzo <42910242+d-Qw4w9WgXc-Q@users.noreply.github.com>
2021-03-08 23:12:57 +0100

    fix constants

--- LuxEngine/Math/Constants.hpp
+++ LuxEngine/Math/Constants.hpp
@@ -4,7 +4,7 @@
 
 
 static constexpr double PI	  = 3.141592653589793239;	//Just PI
-static constexpr double PI2	  = 6.283185307179586478;	//2 * PI
-static constexpr double E	  = 0.577215664901532861;	//Euler's number
+static constexpr double TAU	  = 6.283185307179586478;	//2 * PI
+static constexpr double E	  = 2.718281828459045235;	//Euler's number
 static constexpr double SQRT2 = 1.414213562373095049;	//Pythagoras's constant
 static constexpr double PHI	  = 1.618033988749894848;	//Golden ratio
