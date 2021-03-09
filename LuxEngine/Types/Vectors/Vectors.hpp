c68dba30b001fb38f940dfa515f3c1ef74098f4e
UwU_Snek <xiaoju345@gmail.com>
2021-03-09 18:24:02 +0100

    Fix vectors functions Add sum, sub and sqrt functions

--- LuxEngine/Types/Vectors/Vectors.hpp
+++ LuxEngine/Types/Vectors/Vectors.hpp
@@ -8,6 +8,7 @@
 
 
 
+
 using i8v2   = vec2_t<i8>;      //  Two-dimentional int8 vector
 using i8v3   = vec3_t<i8>;      //Three-dimentional int8 vector
 using i8v4   = vec4_t<i8>;      // Four-dimentional int8 vector
@@ -86,24 +87,39 @@ using float64v4 = vec4_t<f64>;  // Four-dimentional float64 vector
 
 
 
-//TODO use intrinsic functions whenever possible
-//TODO explicit custom type conversion
-static inline constexpr auto dist(const vec2_t<auto>& a, const vec2_t<auto>& b) { return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2)); }										  //Returns the Euclidean distance between 2 vectors
-static inline constexpr auto dist(const vec3_t<auto>& a, const vec3_t<auto>& b) { return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2) + pow(b.z - a.z, 2)); }					  //Returns the Euclidean distance between 2 vectors
-static inline constexpr auto dist(const vec4_t<auto>& a, const vec4_t<auto>& b) { return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2) + pow(b.z - a.z, 2) + pow(b.w - a.w, 2)); } //Returns the Euclidean distance between 2 vectors
 
-static inline constexpr vec2_t<int32> sign(const vec2_t<auto>& v) { return { sign(v.x), sign(v.y) }; }							//Returns a vector containing the sign of the elements of v (-1 if the element is negative, +1 if it's positive or 0)
-static inline constexpr vec3_t<int32> sign(const vec3_t<auto>& v) { return { sign(v.x), sign(v.y), sign(v.z) }; }				//Returns a vector containing the sign of the elements of v (-1 if the element is negative, +1 if it's positive or 0)
-static inline constexpr vec4_t<int32> sign(const vec4_t<auto>& v) { return { sign(v.x), sign(v.y), sign(v.z), sign(v.w) }; }	//Returns a vector containing the sign of the elements of v (-1 if the element is negative, +1 if it's positive or 0)
+static inline constexpr auto sum(const vec2_t<auto>& v) { return v.x + v.y; }
+static inline constexpr auto sum(const vec3_t<auto>& v) { return v.x + v.y + v.z; }
+static inline constexpr auto sum(const vec4_t<auto>& v) { return v.x + v.y + v.z + v.w; }
+
+static inline constexpr auto sub(const vec2_t<auto>& v) { return v.x - v.y; }
+static inline constexpr auto sub(const vec3_t<auto>& v) { return v.x - v.y - v.z; }
+static inline constexpr auto sub(const vec4_t<auto>& v) { return v.x - v.y - v.z - v.w; }
 
 static inline constexpr vec2_t<auto> abs(const vec2_t<auto>& v) { return { abs(v.x), abs(v.y) }; }									//Returns a vector containing the absolute value of the elements of v
 static inline constexpr vec3_t<auto> abs(const vec3_t<auto>& v) { return { abs(v.x), abs(v.y), abs(v.z) }; }							//Returns a vector containing the absolute value of the elements of v
 static inline constexpr vec4_t<auto> abs(const vec4_t<auto>& v) { return { abs(v.x), abs(v.y), abs(v.z), abs(v.w) }; }				//Returns a vector containing the absolute value of the elements of v
 
+static inline constexpr vec2_t<auto> sqrt(const vec2_t<auto>& v) { return { sqrt(v.x), sqrt(v.y) }; }
+static inline constexpr vec3_t<auto> sqrt(const vec3_t<auto>& v) { return { sqrt(v.x), sqrt(v.y), sqrt(v.z) }; }
+static inline constexpr vec4_t<auto> sqrt(const vec4_t<auto>& v) { return { sqrt(v.x), sqrt(v.y), sqrt(v.z), sqrt(v.w) }; }
+
+// static inline constexpr auto dist(const vec2_t<auto>& a, const vec2_t<auto>& b) { return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2)); }										  //Returns the Euclidean distance between 2 vectors
+// static inline constexpr auto dist(const vec3_t<auto>& a, const vec3_t<auto>& b) { return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2) + pow(b.z - a.z, 2)); }					  //Returns the Euclidean distance between 2 vectors
+// static inline constexpr auto dist(const vec4_t<auto>& a, const vec4_t<auto>& b) { return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2) + pow(b.z - a.z, 2) + pow(b.w - a.w, 2)); } //Returns the Euclidean distance between 2 vectors
+
+static inline constexpr auto dist(const vec2_t<auto>& a, const vec2_t<auto>& b) { return sqrt(sum(pow(a - b, 2))); }										  //Returns the Euclidean distance between 2 vectors
+static inline constexpr auto dist(const vec3_t<auto>& a, const vec3_t<auto>& b) { return sqrt(sum(pow(a - b, 2))); }					  //Returns the Euclidean distance between 2 vectors
+static inline constexpr auto dist(const vec4_t<auto>& a, const vec4_t<auto>& b) { return sqrt(sum(pow(a - b, 2))); } //Returns the Euclidean distance between 2 vectors
+
+static inline constexpr vec2_t<int32> sign(const vec2_t<auto>& v) { return { sign(v.x), sign(v.y) }; }							//Returns a vector containing the sign of the elements of v (-1 if the element is negative, +1 if it's positive or 0)
+static inline constexpr vec3_t<int32> sign(const vec3_t<auto>& v) { return { sign(v.x), sign(v.y), sign(v.z) }; }				//Returns a vector containing the sign of the elements of v (-1 if the element is negative, +1 if it's positive or 0)
+static inline constexpr vec4_t<int32> sign(const vec4_t<auto>& v) { return { sign(v.x), sign(v.y), sign(v.z), sign(v.w) }; }	//Returns a vector containing the sign of the elements of v (-1 if the element is negative, +1 if it's positive or 0)
+
 static inline constexpr vec2_t<auto> dist2D(const vec2_t<auto>& a, const vec2_t<auto>& b) { return abs(a - b); }							//Returns a vector containing the distances between the values of v
 static inline constexpr vec3_t<auto> dist3D(const vec3_t<auto>& a, const vec3_t<auto>& b) { return abs(a - b); }							//Returns a vector containing the distances between the values of v
 static inline constexpr vec4_t<auto> dist4D(const vec4_t<auto>& a, const vec4_t<auto>& b) { return abs(a - b); }							//Returns a vector containing the distances between the values of v
 
-static inline constexpr auto dot(const vec2_t<auto>& a, const vec2_t<auto>& b) { return (a.x * b.x) + (a.y * b.y); }									//Returns the dot product of 2 vectors
-static inline constexpr auto dot(const vec3_t<auto>& a, const vec3_t<auto>& b) { return (a.x * b.x) + (a.y * b.y) + (a.z * b.z); }						//Returns the dot product of 2 vectors
-static inline constexpr auto dot(const vec4_t<auto>& a, const vec4_t<auto>& b) { return (a.x * b.x) + (a.y * b.y) + (a.z * b.z) + (a.w * b.w); }		//Returns the dot product of 2 vectors
+static inline constexpr auto dot(const vec2_t<auto>& a, const vec2_t<auto>& b) { return sum(a * b); }									//Returns the dot product of 2 vectors
+static inline constexpr auto dot(const vec3_t<auto>& a, const vec3_t<auto>& b) { return sum(a * b); }						//Returns the dot product of 2 vectors
+static inline constexpr auto dot(const vec4_t<auto>& a, const vec4_t<auto>& b) { return sum(a * b); }		//Returns the dot product of 2 vectors
