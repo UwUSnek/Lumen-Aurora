d004a7bec554113b467b693cda8f6c8cea05741d
UwU_Snek <xiaoju345@gmail.com>
2021-04-07 10:44:16 +0200

    Update InputSurface

--- LuxEngine/Types/LuxObject/2D/InputSurface.hpp
+++ LuxEngine/Types/LuxObject/2D/InputSurface.hpp
@@ -6,10 +6,10 @@
 
 namespace lux::obj{
     virtual struct InputSurface : public Obj2_b{
-        virtual void onClick();
-        virtual void onHover();
-        virtual void onEnter();
-        virtual void onExit();
-        virtual void onAxis();
+        virtual void onClick(const f32v2 vPos, /*FIXME*/ vButton) = 0;
+        virtual void onHover(const f32v2 vPos) = 0;
+        virtual void onEnter(const f32v2 vPos) = 0;
+        virtual void onExit(const f32v2 vPos) = 0;
+        virtual void onAxis(const i32 vAxis) = 0;
     };
 }
