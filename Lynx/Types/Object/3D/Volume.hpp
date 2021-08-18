3341cd93dde2ca2b88d93659b263cd516f135014
UwU_Snek <xiaoju345@gmail.com>
2021-08-18 23:44:08 +0200

    Minor improvements

--- Lynx/Types/Object/3D/Volume.hpp
+++ Lynx/Types/Object/3D/Volume.hpp
@@ -2,6 +2,7 @@
 #define LNX_H_VOLUME
 #include "Lynx/Types/Object/Obj_b.hpp"
 #include "Lynx/Types/Object/3D/Octree.hpp"
+#include "Lynx/shaders/3DTest.hpp"
 #include "Lynx/Types/Containers/RaArray.hpp"
 #include "Lynx/Types/VPointer.hpp"
 
@@ -17,21 +18,25 @@ namespace lnx{
 
     struct Volume : public obj::obj3_b{
     private:
-        RaArray<Octree> hbx_nodes;
+        shd::_3DTest data;
+        RaArray<Octree> hbx_nodes; //TODO ?? remove?
 
     public:
-        Octree hbx;                             //Hitbox
-        vram::ptr<Point, eVRam, eStorage> data;   //GPU data
+        Octree hbx;                                 //Hitbox
+        vram::ptr<Point, eVRam, eStorage> points;   //GPU data
         //TODO Generation data
 
-        Volume(const f32v3 vPos, const f32v3 vRot, const RtArray<Point>& vData) {
+        Volume(const f32v3 vPos, const f32v3 vRot, const RtArray<Point>& vPoints) {
             obj3_b::pos = vPos;
             obj3_b::rot = vRot;
             obj3_b::scl = { 1, 1, 1 };
 
-            for(auto i : vData) hbx_nodes.add({
+            for(auto i : vPoints) hbx_nodes.add({
                 i.pos, i.r, 0, { nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr }, nullptr, {}
             });
+
+
+            // data._data
         }
     };
 }
