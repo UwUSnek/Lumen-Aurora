db238a612f8657cdd9005e77ebf2a997cd59cca3
UwU_Snek <xiaoju345@gmail.com>
2021-09-06 04:33:13 +0200

    Improve build output Add verbosity option to lynxg++

--- Tools/Build/lynxg++.py
+++ Tools/Build/lynxg++.py
@@ -2,16 +2,16 @@ import re, sys, os, subprocess, argparse as ap
 import GlslToCpp
 #python3.9 -m py_compile lynxg++.py && { python3 -m PyInstaller -F --clean ./lynxg++.py; cp ./dist/lynxg++ ./; rm -r ./dist; rm ./build -r; rm ./lynxg++.spec; }
 
-#TODO create Lynx/Build, Lynx/Build/Linux and Lynx/Build/Windows directories
 #TODO check if the user build actually has arguments
 
 
 
 p = ap.ArgumentParser(prog = 'lynxg++', add_help = False, usage = 'lynxg++ -m=<mode> [<options...>] -<selector>: <g++ arguments...> <GLSL files...>')
 
-p.add_argument('-h', '--help',   action = 'store_true', dest = 'h')
-p.add_argument('-m', '--mode',   action = 'store',      dest = 'm', choices = ['wd', 'wr', 'ld', 'lr'], )
-p.add_argument('-e', '--engine', action = 'store_true', dest = 'e')
+p.add_argument('-h', '--help',      action = 'store_true', dest = 'h')
+p.add_argument('-m', '--mode',      action = 'store',      dest = 'm', choices = ['wd', 'wr', 'ld', 'lr'])
+p.add_argument('-e', '--engine',    action = 'store_true', dest = 'e')
+p.add_argument('-v', '--verbosity', action = 'store',      dest = 'v', choices = [0, 1, 2, 3], default = 1, type = int)
 
 p.add_argument('-a:', '--always:',  action = 'extend', nargs = '+', default = [], dest = 'a')
 p.add_argument('-d:', '--debug:',   action = 'extend', nargs = '+', default = [], dest = 'd')
@@ -19,33 +19,38 @@ p.add_argument('-r:', '--release:', action = 'extend', nargs = '+', default = []
 p.add_argument('-w:', '--window:',  action = 'extend', nargs = '+', default = [], dest = 'w')
 p.add_argument('-l:', '--linux:',   action = 'extend', nargs = '+', default = [], dest = 'l')
 
-p.add_argument('-wd:',             action = 'extend', nargs = '+', default = [], dest = 'wd')
-p.add_argument('-wr:',             action = 'extend', nargs = '+', default = [], dest = 'wr')
-p.add_argument('-ld:',             action = 'extend', nargs = '+', default = [], dest = 'ld')
-p.add_argument('-lr:',             action = 'extend', nargs = '+', default = [], dest = 'lr')
+p.add_argument('-wd:',              action = 'extend', nargs = '+', default = [], dest = 'wd')
+p.add_argument('-wr:',              action = 'extend', nargs = '+', default = [], dest = 'wr')
+p.add_argument('-ld:',              action = 'extend', nargs = '+', default = [], dest = 'ld')
+p.add_argument('-lr:',              action = 'extend', nargs = '+', default = [], dest = 'lr')
+
 
 #Enumerate g++/glslc arguments
 for i in range(1, len(sys.argv)):
-    selectors = list(set(p._option_string_actions.keys()) - set(['-m', '--mode']))
-    if not sys.argv[i] in selectors and re.match(r'^((-m)|(--mode))=.*$', sys.argv[i]) == None:
+    selectors = list(set(p._option_string_actions.keys()) - set(['-m', '--mode', '-v', '--verbosity']))
+    if(
+        not sys.argv[i] in selectors and
+        re.match(r'^((-m)|(--mode))=.*$', sys.argv[i]) == None and
+        re.match(r'^((-v)|(--verbosity))=.*$', sys.argv[i]) == None
+    ):
         sys.argv[i] = str(i).zfill(5) + sys.argv[i]
+
+
 #FIXME print an error if there are no selectors before the first unknown option
 
 #TODO add automatic cleanup before compilation
-#TODO move build stage name to the center of the divider
-
 #TODO replace hard coded paths and use configuration file
 
 #TODO add verbosity option. default: 1
 #TODO -v=0: only show errors
-#TODO -v=1: + actual gcc command
-#TODO       + build stage dividers,
+#     -v=1: + actual gcc command
+#           + build stage dividers,
 #TODO       + number of compiled shaders
 #TODO       + [total progress bar]
 #TODO       + build outcome
-#TODO -v=2: + "compiling shader ..." for each shader
+#     -v=2: + "compiling shader ..." for each shader
 #TODO       + [per-stage progress bar]
-#TODO -v=3: + actual spirv-val and glslangValidator commands and generated .cpp and .hpp for each shader
+#     -v=3: + actual spirv-val and glslangValidator commands and generated .cpp and .hpp for each shader
 #TODO       + [per-shader & per-file progress bar (multiple bars if multithreaded)]
 
 #TODO add command line build progress bar in the last line
@@ -145,7 +150,7 @@ while i < len(cmd):
     if r != None:
         cmd[i] += ';' + r.group(1) + '.spv'
 
-    r = re.match(r'^(.*)\.comp;(.*)\.spv$', cmd[i]) #TODO actually use the output name
+    r = re.match(r'^(.*)\.comp;(.*)\.spv$', cmd[i]) #TODO allow the user to modify interface output files
     if r != None:
         cmdsh += [[r.group(1), r.group(2)]]
         del(cmd[i])
@@ -184,14 +189,15 @@ if not args.e: cmd += [                                         #When building u
 
 
 
-def runCmd(args):
+def runCmd(args, v:int):
     output = ''
     try:
         output = subprocess.check_output(args, universal_newlines=True)
-        print('\033[1m' + (' '.join(args)) + '\033[0m')
+        if v == 3:
+            print('\033[35m' + (' '.join(args)) + '\033[0m')
     except subprocess.CalledProcessError as e:
         print(
-            'The task \033[1m"' + ' '.join(args) +
+            'lynxg++: fatal error: the task \033[1m"' + ' '.join(args) +
             '"\033[0m exited with return code ' + str(e.returncode) +
             ':\n\033[31m' + str(output) + '\033[0m'
         )
@@ -199,23 +205,43 @@ def runCmd(args):
 
 
 
+
+tsize = os.get_terminal_size().columns
+def center(stage):
+    h = '-' * (int(tsize / 2) - int(len(stage) / 2) - 3 - 1)
+    return f"\n\n\033[1m|{ h }[ { stage } ]{ h }|\033[0m\n\n"
+
+
+
+
 #Run GLSLC commands
 if len(cmdsh) > 0:
-    print('\n' + ('-' * os.get_terminal_size().columns))
-    print('\n\n' '\033[1m' 'COMPILING SHADERS\033[0m')
+    if args.v >= 1:
+        print(center('Compiling shaders'))
 
-    for files in cmdsh:
-        print('\n')
-        runCmd(['glslangValidator', '-V', files[0] + '.comp', '-o', files[1] + '.spv'])
-        runCmd(['spirv-val', files[0] + '.spv'])
+    for i, files in enumerate(cmdsh):
+        if args.v >= 2:
+            if args.v == 3: print('')
+            s = 'Compiling shader "' + files[0] + '"...'
+            print(s + (' ' * (tsize - len(s) - 4 - 1 - 4)) + str(i + 1).rjust(4) + '/' + str(len(cmdsh)).ljust(4))
+        # if args.v >= 1:
+            # print(str(i + 1) + '/' + str(len(cmdsh)), end = '\r')
+
+        runCmd(['glslangValidator', '-V', files[0] + '.comp', '-o', files[1] + '.spv'], args.v)
+        runCmd(['spirv-val', files[0] + '.spv'], args.v)
+        if args.v == 3:
+            print('\033[35mGenerating interface files...\033[0m')
         r = GlslToCpp.parseShader(files[0] + '.comp', _pf)
         if r != 0: sys.exit(r)
     print('\n')
 
 
+
+
 #Run G++ command
 if len(cmd) > 1:
-    print('\n' + ('-' * os.get_terminal_size().columns))
-    print('\n\n' '\033[1m' 'COMPILING TRANSLATION UNITS\033[0m')
-    print(' '.join(cmd) + '\n' '\033[0m')
+    if args.v >= 1:
+        print(center('Compiling translation units'))
+        print('\n')
+        print('\033[35m' + (' '.join(cmd)) + '\n\033[0m')
     subprocess.run(cmd)
